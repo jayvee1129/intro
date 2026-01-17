eaacf5587529fcadd1e38a5bd28517346891832e
jayvee1129 <jvpsalac.student@ua.edu.ph>
2026-01-17 23:11:47 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,30 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+ 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Greetings and welcome to my website! I'm John Victor Salac, A 3rd year BSIT-3A Student at the University of the Assumption.</p>
+        <p>
+        <h3>When you think about a "perfect" website, are you more drawn to how it (UI/UX, animations, layouts) or how it (databases, user authentication, logic)? Explain why.</h3>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+A perfect website to me, is one that will have strong back end functionality as well as smooth front end features. Even if a website is visually appealing with complex animations, its true potential will be lost if it runs slowly. Proper database management, a logical system flow, and secure user authentication will keep a website from losing value. A good design is not the only thing that makes a website high quality and valuable. Strong functionality will keep a website from falling to the wayside and making it a website that runs efficiently, and meets the needs of the user.
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+<h3>Are there specific technologies or frameworks you are eager to learn (e.g., React, Tailwind CSS, Node.js, Next.js)?</h3>
+Because of my strong interest in these technologies, I am extremely excited to be learning about React, Node.js and Next.js. React excites me as it is perfect for building dynamic and interactive user interfaces. Node.js also excites me as it is perfect for handling efficient server-side logic. Next.js is the one that excites me the most as it brings together server-side logic and dynamic user interface design. Learning these technologies is exactly what I have been hoping to do in order to understand every end of a website.
+
+<h3>What is one specific feature you’ve seen on a modern website that you’ve always wondered how to build?</h3>
+A feature I have always found interesting and wanted to learn to build is real-time updates on websites. Live notifications and content changes without refreshing the page are intriguing. Live chats and real-time dashboards are examples of how data is sent and received. I wanted to learn how interactive web applications work and this is one version of that.<br/>
+
+          <br/>(This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      </section>
+    </Layout>
   );
 }
